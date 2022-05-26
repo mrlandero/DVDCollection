d5cba02a82c1e353329eff25576c7f1d3720de80
Tony <mr.landero@gmail.com>
2022-05-26 13:00:58 -0500

    Create DVD java class and add the fields

---
+++ DVD.java
@@ -0,0 +1,11 @@
+public class DVD {
+
+    //Fields:
+    private String title; //Title of this DVD
+    private String releaseDate; //Release date of this DVD
+    private String rating; //MPAA rating of this DVD
+    private String director; //Director of this DVD
+    private String studio; //The studio of this DVD
+    private String userMessage; //Message left for this DVD
+
+}
