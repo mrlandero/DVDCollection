8c2b52db1dba1fd0aa90ef3fc21e6fca0dee435a
Tony <mr.landero@gmail.com>
2022-05-26 13:33:50 -0500

    Finalize DVD class / Test the object in Main class

--- DVD.java
+++ DVD.java
@@ -6,6 +6,61 @@ public class DVD {
     private String rating; //MPAA rating of this DVD
     private String director; //Director of this DVD
     private String studio; //The studio of this DVD
-    private String userMessage; //Message left for this DVD
+
+    public DVD(String dvdTitle, String dvdReleaseDate, String dvdRating, String dvdDirector, String dvdStudio) {
+        setTitle(dvdTitle);
+        setReleaseDate(dvdReleaseDate);
+        setRating(dvdRating);
+        setDirector(dvdDirector);
+        setStudio(dvdStudio);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setTitle(String newTitle) {
+        title = newTitle.toUpperCase();
+    }
+
+    public void setReleaseDate(String newReleaseDate) {
+        releaseDate = newReleaseDate;
+    }
+
+    public void setRating(String newRating) {
+        rating = newRating;
+    }
+
+    public void setDirector(String newDirector) {
+        director = newDirector;
+    }
+
+    public void setStudio(String newStudio) {
+        studio = newStudio;
+    }
+
+    public String toString() {
+        return "DVD Title: " + this.title + "\n"
+        + "DVD Release Date: " + this.releaseDate + "\n"
+        + "DVD Rating: " + this.rating + "\n"
+        + "DVD Director: " + this.director + "\n"
+        + "DVD Studio: " + this.studio + "\n";
+    }
 
 }
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        DVD dvd1 = new DVD("Movie 1", "1/1/00", "R", "Tony", "MGM");
+
+        System.out.println(dvd1);
+    }
+}
