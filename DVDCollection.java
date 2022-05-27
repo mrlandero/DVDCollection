59b644e5c588e116483c50d940e4864523a7da09
Tony <mr.landero@gmail.com>
2022-05-27 09:45:32 -0500

    Add adding/modifying/removing functionality to the DVDCollection class

--- DVDCollection.java
+++ DVDCollection.java
@@ -46,6 +46,69 @@ public class DVDCollection {
     public void addOrModifyDVD(String title, String releaseDate, String rating, String director, String studio) {
         //If the array is full and a new DVD needs to be added,
         //double the size of the array first
-        if(validRating(rating))
+        if(!validRating(rating)) {
+            return;
+        }
+        int runTime = validateRunningTime(runningTime);
+        if(runTime <= 0) {
+            return;
+        }
+
+        modified = true;
+
+        title = title.toUpperCase(); //Forcing the user input to be upper cased
+
+        //Search for existing element
+        int index = findDVD(title);
+        if(index >= 0) { //dvd exists in collection, update it
+            dvdArray[index].setTitle(title);
+            dvdArray[index].setRating(rating);
+            dvdArray[index].setRunningTime(runTime);
+            System.out.println("Found duplicate at index " + index);
+        }
+        else { //dvd not in collection, add it
+            if(numdvds < dvdArray.length) {
+                dvdArray[numdvds] = new DVD(title, rating, runTime);
+                System.out.println("[" + numdvds + "]" + dvdArray[numdvds].toString());
+                numdvds++;
+                Arrays.sort(dvdArray, 0, numdvds, new DVDTitleSort());
+            }
+            else {
+                //Todo double array size and try again
+                System.out.println("We need to increase the array size first");
+                doubleDVDArraySize();
+                System.out.println(toString());
+                //addOrModifyDVD(title, rating, runningTime);
+                //System.out.println(toString);
+            }
+        }
+    }
+
+    public void removeDVD(String title) {
+        int index = findDVD(title);
+        if(index >= 0) {
+            modified = true;
+            DVD[] newArray = new DVD[dvdArray.length - 1];
+            System.arraycopy(dvdArray, 0, newArray, 0, index);
+            System.arraycopy(dvdArray, index + 1, newArray, index, dvdArray.length - index - 1);
+            dvdArray = newArray;
+            numdvds--;
+        }
+        else {
+            //Todo: Throw exception
+            System.out.println("DVD not found");
+        }
+    }
+
+    public String getDVDsByRating(String rating) {
+        rating = rating.toUpperCase();
+        String list = "";
+
+        for(int i = 0; i < numdvds; i++) {
+            if(dvdArray[i].getRating().equals(rating) {
+                list += (dvdArray[i].toString() + '\n');
+            }
+        }
+        return list;
     }
 }
